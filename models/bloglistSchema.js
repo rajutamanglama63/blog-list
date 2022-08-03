6245137ddba75a7ba61cac6cff283ed114ae4e2a
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-03 13:44:24 +0545

    feat: bloglistSchema get set with new functionaliy

--- models/bloglistSchema.js
+++ models/bloglistSchema.js
@@ -32,9 +32,9 @@ const bloglistSchema = new mongoose.Schema({
 
 bloglistSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject._id = returnedObject._id.toString();
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
-    delete returnedObject._v;
+    delete returnedObject.__v;
   },
 });
 
