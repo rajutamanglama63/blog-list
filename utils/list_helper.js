21a0c426ea808611d06930cdf478738700377c84
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-03 16:54:36 +0545

    feat: total likes testing done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogspostlist) => {
+  // VANILLA APPROACH
+  //   let numOfLikes = 0;
+  //   for (let i = 0; i < blogspostlist.length; i++) {
+  //     numOfLikes += blogspostlist[i].likes;
+  //   }
+
+  //   return numOfLikes;
+
+  // ADVANCE APPROACH
+  return blogspostlist.reduce((a, b) => {
+    let totalNumOfLikes = a + b.likes;
+    return totalNumOfLikes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
