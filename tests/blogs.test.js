3a0e6e235e8db2f7947d3a67c23a88a7e0928fb1
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-04 12:15:24 +0545

    feat: favoriteBlog test done

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -23,7 +23,7 @@ describe("total likes", () => {
     expect(result).toBe(5);
   });
 
-  test("when list has multiple blog, equals the most likes obj", () => {
+  test("when list has multiple blog, equals total likes all obj", () => {
     const listwithMultipleBlog = [
       {
         _id: "5a422aa71b54a676234d17f8",
@@ -45,5 +45,37 @@ describe("total likes", () => {
     ];
     const result = listHelper.totalLikes(listwithMultipleBlog);
     expect(result).toBe(20);
+  });
+
+  test("show most liked blog as favorite blog", () => {
+    const blogslist = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Science & Technology",
+        author: "Raju Lama",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 15,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.favoriteBlog(blogslist);
+    expect(result).toEqual({
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Science & Technology",
+      author: "Raju Lama",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 15,
+      __v: 0,
+    });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -21,17 +21,19 @@ const totalLikes = (blogspostlist) => {
 const favoriteBlog = (blogslist) => {
   let max = 0;
   for (let i = 0; i < blogslist.length; i++) {
-    if (blogslist[i].likes < max) {
+    if (blogslist[i].likes > max) {
       max = blogslist[i].likes;
     }
   }
 
   // console.log(max)
-  const myObj = blogspostlist.find((a) => {
+  const myObj = blogslist.find((a) => {
     // console.log(a.likes)
     return a.likes === max;
   });
-  // console.log(myObj)
+  //   console.log(myObj)
+
+  return myObj;
 };
 
 module.exports = {
