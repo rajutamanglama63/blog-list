84ca950b466afab322959ae6e5a6d75d7856053b
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-06 11:59:45 +0545

    feat: change made in scripts

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI =
+  process.env.NODE_ENV === "tesst"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGO_URI;
 
 module.exports = {
   PORT,
