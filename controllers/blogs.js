c294a11aa77c453310e62859669a94b03944740d
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-08 22:53:30 +0545

    feat: post request changed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const Blog = require("../models/bloglistSchema");
+const User = require("../models/userSchema");
 
 const blogsRouter = express.Router();
 
-blogsRouter.get("/", (req, res, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      res.status(200).json(blogs);
-    })
-    .catch((err) => next(err));
+blogsRouter.get("/", async (req, res, next) => {
+  // Blog.find({})
+  //   .populate("user", { username: 1, name: 1 })
+  //   .then((blogs) => {
+  //     res.status(200).json(blogs);
+  //   })
+  //   .catch((err) => next(err));
+
+  try {
+    const allBlogs = await Blog.find({}).populate("user", {
+      username: 1,
+      name: 1,
+    });
+
+    res.status(200).json(allBlogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogsRouter.post("/", (req, res, next) => {
+blogsRouter.post("/", async (req, res, next) => {
   if (req.body.likes === undefined) {
     req.body.likes = 0;
   }
+
+  const user = await User.findById(req.body.userId);
 
   if (!(req.body.title && req.body.url)) {
     res.status(400).end();
@@ -24,13 +39,18 @@ blogsRouter.post("/", (req, res, next) => {
       author: req.body.author,
       url: req.body.url,
       likes: req.body.likes,
+      userId: user._id,
     });
-    blog
-      .save()
-      .then((result) => {
-        res.status(201).json(result);
-      })
-      .catch((err) => next(err));
+
+    try {
+      const newBlog = await blog.save();
+      user.blogs.concat(newBlog._id);
+      await user.save();
+
+      res.status(200).json(newBlog);
+    } catch (error) {
+      next(error);
+    }
   }
 });
 
