6a432158497e56c5952a75412b45d1b6c40ef2e9
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-10 13:29:17 +0545

    feat: created new component notification

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import Login from "./components/Login";
+import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -12,7 +14,8 @@ const App = () => {
 
   return (
     <div>
-      <Login />
+      <Notification message={message} />
+      <Login setMessage={setMessage} />
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
