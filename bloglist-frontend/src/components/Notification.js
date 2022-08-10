200da5cd61d19b79369f3cf01b0e7680ad9630e4
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-10 21:54:09 +0545

    feat: set classname for message styling

--- bloglist-frontend/src/components/Notification.js
+++ bloglist-frontend/src/components/Notification.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 const Notification = ({ message }) => {
+  let msgStyle = "";
+  if (message === null) {
+    return null;
+  }
+
+  if (message === "User logged in successfully.") {
+    msgStyle = "green";
+  } else if (
+    message === "Invalid credentials" ||
+    message === "User does not exist."
+  ) {
+    msgStyle = "red";
+  }
   return (
     <div>
       <p>{message}</p>
