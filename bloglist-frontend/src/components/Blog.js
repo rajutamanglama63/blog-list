68eac6a15f34c081daeb63ebb1fdb64c2c20b128
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-11 07:08:41 +0545

    feat: listing of blog fixed properly

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,17 @@
-const Blog = ({ blog, user }) => (
+const Blog = ({ blogs, user }) => (
   <div>
     {/* {blog.title} {blog.author} */}
     <p>{user.name} logged in</p>
 
-    <ul>
+    {/* <ul>
       {user.blogs.map((blog) => (
         <li key={blog.id}>blog</li>
+      ))}
+    </ul> */}
+
+    <ul>
+      {blogs.map((blog) => (
+        <li key={blog.id}>{blog.title}</li>
       ))}
     </ul>
   </div>
