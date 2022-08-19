c5c991b82e30bfc4897e7ef2a6c2a53a74b86b56
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-19 12:58:27 +0545

    feat: firt test pass by using different approach

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -6,22 +6,18 @@ import Blog from "./Blog";
 test("renders title and author", () => {
   const blog = {
     title: "title of the blog",
-  };
-
-  render(<Blog blog={blog} />);
-
-  const element = screen.getByText("title of the blog");
-
-  expect(element).toBeDefined();
-});
-
-test("renders author", () => {
-  const blog = {
     author: "author of the blog",
   };
 
-  const { container } = render(<Blog blog={blog} />);
+  // render(<Blog blog={blog} />);
 
+  // const element = screen.getByText("title of the blog");
+
+  // expect(element).toBeDefined();
+
+  const { container } = render(<Blog blog={blog} />);
   const div = container.querySelector(".blog");
+
+  expect(div).toHaveTextContent("title of the blog");
   expect(div).toHaveTextContent("author of the blog");
 });
